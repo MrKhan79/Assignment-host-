ee190eac8afd42e8b6a20927be28fef1caab9acf
Sahil Khan <sahilkhan797491@gmail.com>
2023-11-28 16:49:23 +0530

    first commit

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, './build')));
+// app.use(express.static(path.join(__dirname, './build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './build/index.html'));
+// });
 
 const User = require("./models/User");
 
